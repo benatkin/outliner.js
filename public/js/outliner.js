226d6d34d08a3e8311b1d082e13d74f39b4c844c
ben@benatkin.com <ben@benatkin.com>
2010-08-15 12:08:49 -0600

    improved appearance and implemented "hide empty"

--- public/js/outliner.js
+++ public/js/outliner.js
@@ -1,7 +1,26 @@
 (function($) {
+  var removeEmpty = function(data) {
+    if (data != null && typeof data == "object") {
+      var empty = true;
+      $.each(data, function(key, value) {
+        if (removeEmpty(value) == false || value == null || value == '""') {
+          delete data[key];
+        } else {
+          empty = false;
+        }
+      });
+      if (empty) return false;
+    }
+    return true;
+  }
+
+  var addEmpty = function(container, text) {
+    var span = $('<span/>').appendTo(container).text(text).addClass('empty');
+  }
+
   var render_ul = function(container, data, options) {
     if (data == null) {
-      container.text('null');
+      addEmpty(container, 'null');
     } else if (typeof data == "object") {
       var ul = $('<ul/>').appendTo(container);
       var empty = true;
@@ -13,10 +32,14 @@
         empty = false;
       });
       if (empty) {
-        container.text(data.push ? '[]' : '{}');
+        container.children().remove();
+        addEmpty(container, data.push ? '[]' : '{}');
       }
     } else {
-      container.text(data);
+      if (empty == "")
+        addEmpty(container, '""');
+      else
+        container.text(data);
     }
   }
 
@@ -24,7 +47,10 @@
     'raw': function(container, options) {
       container.html('<textarea>' + JSON.stringify(options.data, undefined, 2) + '</textarea>');
     },
-    'ul': function(container, options) { return render_ul(container, options.data, options) }
+    'ul': function(container, options) {
+      if (options.hideEmpty) removeEmpty(options.data);
+      render_ul(container, options.data, options)
+    }
   };
 
   $.fn.outliner = function(options) {
