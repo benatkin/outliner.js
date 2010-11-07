73441a53a06bf49260d640eb7a315691276cc61c
ben@benatkin.com <ben@benatkin.com>
2010-11-06 19:22:48 -0600

    remove alternate renderers

--- js/outliner.js
+++ js/outliner.js
@@ -82,50 +82,9 @@
     return true;
   }
 
-  var addEmpty = function(container, text) {
-    var span = $('<span/>').appendTo(container).text(text).addClass('empty');
-  }
-
-  var render_ul = function(container, data, options) {
-    if (data == null) {
-      addEmpty(container, 'null');
-    } else if (typeof data == "object") {
-      var ul = $('<ul/>').appendTo(container);
-      var empty = true;
-      $.each(data, function(key, value) {
-        var li = $('<li/>').appendTo(ul);
-        render_ul(li, value, options);
-        var keySpan = $('<span/>').prependTo(li);
-        keySpan.text(key).addClass("key");
-        empty = false;
-      });
-      if (empty) {
-        container.children().remove();
-        addEmpty(container, data.push ? '[]' : '{}');
-      }
-    } else {
-      if (empty == "")
-        addEmpty(container, '""');
-      else
-        container.text(data);
-    }
-  }
-
-  var renderers = {
-    'raw': function(container, options) {
-      container.html('<textarea>' + JSON.stringify(options.data, undefined, 2) + '</textarea>');
-    },
-    'ul': function(container, options) {
-      render_ul(container, options.data, options)
-    },
-    'div': function(container, options) {
-      container.appendCollection('root', options.data);
-    }
-  };
-
   $.fn.outliner = function(options) {
     this.html('');
     if (options.hideEmpty) removeEmpty(options.data);
-    renderers[options.style](this, options);
+    this.appendCollection('root', options.data);
   };
 })(jQuery);
