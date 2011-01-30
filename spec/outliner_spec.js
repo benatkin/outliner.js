65565b61e844eaf379de0b40c0afd0624404a16b
Ben Atkin <ben@benatkin.com>
2011-01-29 17:11:51 -0700

    set up jasmine

---
+++ spec/outliner_spec.js
@@ -0,0 +1,9 @@
+(function() {
+  describe('path and json types', function() {
+    describe('find', function() {
+      it("runs a spec", function() {
+        expect("").toEqual("this should fail");
+      });
+    });
+  });
+}).call(this);
