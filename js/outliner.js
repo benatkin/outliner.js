96fbfb3173a02b6d27694c5ce70b7f936a787d29
Ben Atkin <ben@benatkin.com>
2011-07-18 13:40:48 -0600

    make naturalSort optional

--- js/outliner.js
+++ js/outliner.js
@@ -1,10 +1,15 @@
 (function($) {
   $.outliner = function($el, options) {
+    this.options = $.extend({}, this.defaults, options);
+    console.log(options);
     $el.html('').addClass('outliner');
-    this.appendCollection($el, 'root', options.data);
+    this.appendCollection($el, 'root', this.options.data);
   }
 
   $.outliner.fn = $.outliner.prototype = {
+    defaults: {
+      naturalSort: true
+    },
     // determine types
     nodeType: function(value) {
       return (value != null && typeof value === "object")
@@ -33,7 +38,10 @@
             keys.push(key);
           }
         }
-        keys.sort(this.naturalSort);
+        if (this.options.naturalSort === true && $.naturalSort !== 'undefined')
+          keys.sort($.naturalSort);
+        else
+          keys.sort();
       } else {
         keys = value;
       }
@@ -80,48 +88,6 @@
       if (leafType == "string") {
         valueElem.html(valueElem.html().replace("\n", "<br />"));
       }
-    },
-
-    /*
-     * Natural Sort algorithm for Javascript - Version 0.6 - Released under MIT license
-     * Author: Jim Palmer (based on chunking idea from Dave Koelle)
-     * Contributors: Mike Grier (mgrier.com), Clint Priest, Kyle Adams, guillermo
-    */
-    naturalSort: function (a, b) {
-      var re = /(^-?[0-9]+(\.?[0-9]*)[df]?e?[0-9]?$|^0x[0-9a-f]+$|[0-9]+)/gi,
-         sre = /(^[ ]*|[ ]*$)/g,
-         dre = /(^([\w ]+,?[\w ]+)?[\w ]+,?[\w ]+\d+:\d+(:\d+)?[\w ]?|^\d{1,4}[\/\-]\d{1,4}[\/\-]\d{1,4}|^\w+, \w+ \d+, \d{4})/,
-         hre = /^0x[0-9a-f]+$/i,
-         ore = /^0/,
-      // convert all to strings and trim()
-      x = a.toString().replace(sre, '') || '',
-      y = b.toString().replace(sre, '') || '',
-      // chunk/tokenize
-      xN = x.replace(re, '\0$1\0').replace(/\0$/,'').replace(/^\0/,'').split('\0'),
-      yN = y.replace(re, '\0$1\0').replace(/\0$/,'').replace(/^\0/,'').split('\0'),
-      // numeric, hex or date detection
-      xD = parseInt(x.match(hre)) || (xN.length != 1 && x.match(dre) && Date.parse(x)),
-      yD = parseInt(y.match(hre)) || xD && y.match(dre) && Date.parse(y) || null;
-      // first try and sort Hex codes or Dates
-      if (yD)
-        if ( xD < yD )       return -1;
-        else if ( xD > yD )  return 1;
-      // natural sorting through split numeric strings and default strings
-      for(var cLoc=0, numS=Math.max(xN.length, yN.length); cLoc < numS; cLoc++) {
-        // find floats not starting with '0', string or 0 if not defined (Clint Priest)
-        oFxNcL = !(xN[cLoc] || '').match(ore) && parseFloat(xN[cLoc]) || xN[cLoc] || 0;
-        oFyNcL = !(yN[cLoc] || '').match(ore) && parseFloat(yN[cLoc]) || yN[cLoc] || 0;
-        // handle numeric vs string comparison - number < string - (Kyle Adams)
-        if (isNaN(oFxNcL) !== isNaN(oFyNcL)) return (isNaN(oFxNcL)) ? 1 : -1; 
-        // rely on string comparison if different types - i.e. '02' < 2 != '02' < '2'
-        else if (typeof oFxNcL !== typeof oFyNcL) {
-          oFxNcL += ''; 
-          oFyNcL += ''; 
-        }
-        if (oFxNcL < oFyNcL) return -1;
-        if (oFxNcL > oFyNcL) return 1;
-      }
-      return 0;
     }
   };
 
