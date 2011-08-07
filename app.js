e453e09a0d0d41a54b2db466e7b209d3a268efc0
Ben Atkin <ben@benatkin.com>
2011-08-06 23:22:49 -0600

    convert to couchapp

---
+++ app.js
@@ -0,0 +1,24 @@
+var couchapp = require('couchapp'),
+    path     = require('path');
+
+ddoc = {
+  _id: '_design/outliner',
+  rewrites: [
+    { from: "/",           to: 'index.html' },
+    { from: "/outliner",   to: '../../'     },
+    { from: "/outliner/*", to: '../../*'    },
+    { from: "/*",          to: '*'          }
+  ]
+};
+
+ddoc.views = {};
+
+ddoc.validate_doc_update = function (newDoc, oldDoc, userCtx) {   
+  if (newDoc._deleted === true && userCtx.roles.indexOf('_admin') === -1) {
+    throw "Only admin can delete documents on this database.";
+  }
+}
+
+couchapp.loadAttachments(ddoc, path.join(__dirname, 'attachments'));
+
+module.exports = ddoc;
