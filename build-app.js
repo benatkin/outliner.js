b760a7fbd8bce72d99672c6a8663d80a96eb16aa
Ben Atkin <ben@benatkin.com>
2011-08-07 19:50:07 -0600

    *use* shimify - it is a middleware

--- build-app.js
+++ build-app.js
@@ -1,8 +1,11 @@
 var browserify = require('browserify'),
     fs         = require('fs');
 
-var bundle = browserify({
-	require: ['seq', 'traverse', 'hashish', 'shimify', 'chainsaw']
-});
+var bundle = browserify()
+             .use(require('shimify'))
+             .require('seq')
+             .require('traverse')
+             .require('hashish')
+             .require('chainsaw');
 
 fs.writeFileSync('attachments/vendor/bundle.js', bundle.bundle());
